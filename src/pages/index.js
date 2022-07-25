d4c903d6b01ebfc591f04b4f7e7bb0c25b131df5
Terje89 <terje@sakariassen.me>
2022-07-25 21:47:53 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,6 @@ const BlogIndex = ({ data, location }) => {
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
-          const {timeToRead} = node
 
           return (
             <li key={post.fields.slug}>

23c905ae16127a7f1c90fedce014d409c7ca9b0e
Terje89 <terje@sakariassen.me>
2022-07-26 20:13:22 +0200

    Update newsletter.js

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,5 +1,5 @@
  import * as React from "react"
- import { useStaticQuery, graphql } from "gatsby"
+ import React, { useState } from "react";
  import { StaticImage } from "gatsby-plugin-image"
  import addToMailchimp from 'gatsby-plugin-mailchimp'
  

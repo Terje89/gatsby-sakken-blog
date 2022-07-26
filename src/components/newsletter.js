8dc2f2faa89680cabb8fc83071b579db1180c928
Terje89 <terje@sakariassen.me>
2022-07-26 10:06:24 +0200

    update

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -3,27 +3,13 @@
  import { StaticImage } from "gatsby-plugin-image"
  
  const Newsletter = () => {
-   const data = useStaticQuery(graphql`
-     query BioQuery {
-        $id: String!
-        markdownRemark(id: { eq: $id }) {
-            id
-            excerpt(pruneLength: 160)
-            html
-            frontmatter {
-              title
-              date(formatString: "DD.MM.YYYY")
-              description
-              timeToRead
-            }
-          }
-     }
-   `)
- 
    return (
      <div className="CardContainer">
+        <div className="DetailsContainer">
+        Details
+         </div>
         <div className="SideImageContainer">
-        test
+        Bilde
          </div>
      </div>
    )

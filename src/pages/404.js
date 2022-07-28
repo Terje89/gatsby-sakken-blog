7289d2942567107bde1cb525e024a23f07e29ec7
Terje89 <terje@sakariassen.me>
2022-07-28 09:18:38 +0200

    test

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,14 +12,7 @@ const NotFoundPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
           <h1>404</h1>
-          <StaticImage
-                layout="fixed"
-                formats={["auto", "webp", "avif"]}
-                src="../images/error.gif"
-                object-fit="scale-down"
-                object-position="50% 50%"
-                alt="404"
-            />
+
           <h2>
           Ser ut som du har gått deg vill
           </h2>

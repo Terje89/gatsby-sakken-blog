96b9d3e605defac7c581117b80fa768a137e9a2d
Terje89 <terje@sakariassen.me>
2023-02-13 10:59:17 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -112,7 +112,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         })
       })
     }
-  }
+  
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
